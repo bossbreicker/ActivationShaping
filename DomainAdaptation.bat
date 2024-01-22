41d415d38c86d158508d9e1af460f6c3ffa05027
bossbreicker <155060680+bossbreicker@users.noreply.github.com>
2024-01-22 12:11:32 +0100

    Add files via upload

---
+++ DomainAdaptation.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Définition de la variable pour le domaine cible
+set target_domain=%1
+
+REM Exécution du script Python avec les arguments
+python main.py ^
+--experiment=DomainAdaptation ^
+--experiment_name=DomainAdaptation/%target_domain%/ ^
+--dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': '%target_domain%'}" ^
+--batch_size=128 ^
+--num_workers=5 ^
+--grad_accum_steps=1
